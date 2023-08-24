df074412854152e86cf9d3ff0e7a92bff7bd0a4a
Naganjali Maguluri <magulurinaganjali@gmail.com>
2023-08-24 10:50:41 -0500

    worksheet

---
+++ practice.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("Hii",10,12.25)
+
+var age = 23
+print("you are\(age) years old and in another \(age) years, you will be \(age * 2)")
+
+var programmingLanguage = "swift"
+print("My favorite programming language is \(programmingLanguage)")
+
+print("the list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: "-")
+
+let pi = 3.14
+print(pi)
+
+
